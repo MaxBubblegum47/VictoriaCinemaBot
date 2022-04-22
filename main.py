5589e56bf00aa3dab51f394f0c8727b0b330409e
maxbubblegum47 <lorenzostigliano@gmail.com>
2022-04-22 11:33:55 +0200

    :fire: Removing redundant code and Victoria_Ciname library now that I use saves

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from curses.ascii import isdigit
+from email import message
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
@@ -6,24 +7,30 @@ from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 import config # for storing bot token
-from Victoria_Cinema import Film
-
-
+import pickle
 
 TOKEN_BOT = config.bot_token #inser your token here
 updater = Updater(TOKEN_BOT, use_context=True)
-    
+
 def start(update: Update, context: CallbackContext):
-    # stampa solamente il primo film  
-    messageOdd = Film.Odd_Movie()
-    messageEven = Film.Even_Movie()
     
-    for elem in messageOdd:
+    messageEvenLoad = []
+    messageOddLoad = []
+    
+    # Load the information from the file
+    with open('saveEven.txt', 'rb') as file:
+        messageEvenLoad = pickle.load(file)
+
+    with open('saveOdd.txt', 'rb') as file:
+        messageOddLoad = pickle.load(file)
+
+
+    for elem in messageOddLoad:
         update.message.reply_text(elem)
     
-    for elem in messageEven:    
+    for elem in messageEvenLoad:    
         update.message.reply_text(elem)
-    
+           
 
 def unknown(update: Update, context: CallbackContext):
     update.message.reply_text(
