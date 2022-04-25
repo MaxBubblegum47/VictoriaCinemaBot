1ecbba67cf124a7496fe635c654e14a8f9b64af1
maxbubblegum47 <lorenzostigliano@gmail.com>
2022-04-25 23:38:08 +0200

    Revert "Revert ":art: Improve the script: it handles more helper script and avoid to loop on bot starting, because it makes it start in background""
    This reverts commit bd78fa43f4576a0ce34b44c06ca7f3d18a6fcbec.

--- container.sh
+++ container.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+
+if [[ -f 'saveEven.txt' ]] & [[ -f 'saveOdd.txt' ]]
+then
+	echo "Save files are present. I'm starting the bot."
+	bash helper_main.sh &
+else
+	echo "There are not save files available. I'm starting Movie.py."
+	bash helper_movie.sh	
+fi
+
+
+
 while true
 do
-	python3 $HOME/Desktop/MovieTheaterBot/Victoria_Cinema/Movie.py
+	echo "Updating Movies"
+	bash helper_movie.sh	
 	sleep 3600
 done
