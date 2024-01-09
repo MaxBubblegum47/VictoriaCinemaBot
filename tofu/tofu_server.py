8ca683ba0d10a48480ac953767bf09981ee08332
MaxBubblegum47 <lorenzostigliano@gmail.com>
2024-01-09 17:58:19 +0100

    adding more stuff

--- tofu/tofu_server.py
+++ tofu/tofu_server.py
@@ -5,10 +5,13 @@ import sqlite3
 from tkinter import *
 from movie import web_scraping, Even_Movie, Odd_Movie
 import os
+import csv
+from db import db_insert_user
 
-@Pyro4.expose
 class Tofu(object):
+    @Pyro4.expose
     def get_film(self):
+        print("funzione get film")
         if os.path.exists("movies.db"):
             print("DB presente")
         else:
@@ -25,12 +28,62 @@ class Tofu(object):
         rows = cursor.fetchall()
         rows.sort(key=lambda e: e[1], reverse=True)
 
+        connection.close()
+
         return rows
     
+    @Pyro4.expose
     def db_update(self):
+        print("funzione update")
         web_scraping()
         Even_Movie()
         Odd_Movie()
+    
+    @Pyro4.expose
+    def db_dump(self):
+        print("funzione dump")
+        if os.path.exists("movies.db"):
+            print("DB presente")
+        else:
+            web_scraping()
+            Even_Movie()
+            Odd_Movie()
+
+        connection = sqlite3.connect("movies.db")
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM example")
+
+        with open('output.csv','w') as out_csv_file:
+            csv_out = csv.writer(out_csv_file)
+            # write header                        
+            csv_out.writerow([d[0] for d in cursor.description])
+            # write data                          
+            for result in cursor:
+              csv_out.writerow(result)
+
+        connection.close()
+    
+    @Pyro4.expose
+    def user_login_or_registration(self, name, password, favourites):
+        print("Sono dentro il server funzione user login registration")
+        print("ecco i dati passatomi dal client: ", name, password, favourites)
+        db_insert_user(name, password, favourites)
+        Tofu.user_print_all(self)
+
+    @Pyro4.expose        
+    def user_print_all(self):
+        print("Sono dentro user print all")
+        connection = sqlite3.connect("users.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        print(rows)
+        connection.close()
+        
+
+
+
         
 def main():
     daemon = Pyro4.Daemon()                # make a Pyro daemon
