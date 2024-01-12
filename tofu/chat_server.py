023ea7a0b1bdd8d87edfdc0b151b96924cb83ee4
MaxBubblegum47 <lorenzostigliano@gmail.com>
2024-01-12 18:35:12 +0100

    adding chat server

--- tofu/chat_server.py
+++ tofu/chat_server.py
@@ -1,33 +1,63 @@
-import sys
-import socket
-import threading
+#!/usr/bin/env python3
+"""Server for multithreaded (asynchronous) chat application."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
 
-#TODO: exit program when client ends the connection
-def connect(conn):
+
+def accept_incoming_connections():
+    """Sets up handling for incoming clients."""
     while True:
-        received = conn.recv(1024)
-        if received ==' ':
-            pass
-        else:
-            print(received.decode())
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
 
-def sendMsg(conn):
+
+def handle_client(client):  # Takes client socket as argument.
+    """Handles a single client connection."""
+
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+
     while True:
-        send_msg = input().replace('b', '').encode()
-        if send_msg == ' ':
-            pass
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name+": ")
         else:
-            conn.sendall(send_msg)
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
 
-if __name__ == '__main__':
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('', 11111))
-    s.listen()
-    (conn, addr) = s.accept() 
-    thread1 = threading.Thread(target = connect, args = ([conn]))
-    thread2 = threading.Thread(target = sendMsg, args = ([conn]))
-    thread1.start()
-    thread2.start()
-    thread1.join()
-    thread2.join()
+
+def broadcast(msg, prefix=""):  # prefix is for name identification.
+    """Broadcasts a message to all the clients."""
+
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8")+msg)
+
+        
+clients = {}
+addresses = {}
+
+HOST = ''
+PORT = 33000
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
